34a829ea86fc00fccfd1d5d4d26a5cce163743db
griesba <griesba.pat@hotmail.com>
2021-04-11 22:37:16 +0200

    code clean

--- parts/card.php
+++ parts/card.php
@@ -3,6 +3,7 @@
     <?php the_post_thumbnail('post-thumbnail', ['class' => 'card-img-top', 'alt' => '', 'style' => 'height: auto;']) ?>
     <div class="card-body">
         <h5 class="card-title"><?php the_title() ?></h5>
+        
         <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
         <?php
         the_terms(get_the_ID(), 'sport', '<li>', '</li><li>', '</li>'); // ici la liste de sport taxonomy
@@ -10,7 +11,7 @@
         <p class="card-text">
             <?php the_excerpt() ?>
         </p>
-        <a href="<?php the_permalink() ?>" class="btn btn-primary">Voir plus</a>
-        <h5><?php the_author() ?></h5>
+        <a href="<?php the_permalink() ?>">Voir plus</a>
+       
     </div>
 </div>
