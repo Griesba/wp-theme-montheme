e963cd678b83d8c1e7b6824eca00c4ee0b69f875
griesba <griesba.pat@hotmail.com>
2021-04-12 23:14:25 +0200

    Creation de widgets

--- footer.php
+++ footer.php
@@ -5,7 +5,19 @@
         'theme_location' => 'footer',
         'container' => false,
         'menu_class' => 'navbar-nav mr-auto'
-        ])?> 
+        ]);
+        the_widget(
+                YoutubeWidget::class, 
+                ['title' => 'Widget avec titre', 'youtube' => 'BZN8INa0aPY'], 
+                ['before_widget' => '', 'after_widget' => '']
+        );
+        the_widget(
+                YoutubeWidget::class, 
+                ['youtube' => 'BZN8INa0aPY'], 
+                ['before_widget' => '', 'after_widget' => '']
+        );
+        ?> 
+    
     
 </footer>
 <div class="row">
