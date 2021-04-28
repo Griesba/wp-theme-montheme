416e614d2a60169609aeaf6cb545053a481e5e25
griesba <griesba.pat@hotmail.com>
2021-04-28 23:57:15 +0200

    adding bootstrap walker for navbar

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
         'container' => false,
         'menu_class' => 'navbar-nav mr-auto'
         ]);
-        the_widget(
+/*         the_widget(
                 YoutubeWidget::class, 
                 ['title' => 'Widget avec titre', 'youtube' => 'BZN8INa0aPY'], 
                 ['before_widget' => '', 'after_widget' => '']
@@ -15,14 +15,14 @@
                 YoutubeWidget::class, 
                 ['youtube' => 'BZN8INa0aPY'], 
                 ['before_widget' => '', 'after_widget' => '']
-        );
+        ); */
         ?> 
     
     
 </footer>
-<div class="row">
+<!-- <div class="row">
         <p><?= get_option('agence_horaire') ?></p>
-  </div>  
+  </div>   -->
 <?php wp_footer() ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -7,7 +7,33 @@
     <?php wp_head() ?>  
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- Brand and toggle button -->
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- End -->
+    <!-- Your website Links -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <?php
+             wp_nav_menu( array(
+             'menu'              => 'primary',
+             'theme_location'    => 'header',
+             'depth'             => 2,
+             'container'         => 'div',
+             'container_class'   => '',
+             'container_id'      => '',
+             'menu_class'        => 'navbar-nav mr-auto',
+             'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+             'walker'            => new wp_bootstrap_navwalker())
+             );
+        ?>
+    </div>
+    <!-- End -->
+</nav>
+
+<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
   <a class="navbar-brand" href="#"><?php bloginfo('name') ?></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -17,9 +43,15 @@
     
     <?php wp_nav_menu([
         'theme_location' => 'header', // header c'est le nom qu'on a donné a notre menu dans register_nav_menu 
-        'container' => false,
-        'menu_class' => 'navbar-nav mr-auto'
-        ])?> 
+        'menu'              => 'primary',
+        'depth'             => 2,
+        'container'         => false,
+        'container_class'   => '',
+        'container_id'      => '',
+        'menu_class'        => 'navbar-nav mr-auto',
+        'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+        'walker'            => new wp_bootstrap_navwalker()
+        ])?>  -->
     
     <!--<ul class="navbar-nav mr-auto">
       <li class="nav-item active">
@@ -43,8 +75,9 @@
         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </li>
     </ul>
+    <?=  get_search_form(); ?>
  -->
-    <?= get_search_form() ?>
+    
   </div>
 </nav>
 
